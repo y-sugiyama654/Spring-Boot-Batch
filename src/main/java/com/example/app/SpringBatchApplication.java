b21485264a97607bcd81655e127392636582584e
Yuta <y.sugiyama54@gmail.com>
2022-11-06 19:04:15 +0900

    config設定

--- src/main/java/com/example/app/SpringBatchApplication.java
+++ src/main/java/com/example/app/SpringBatchApplication.java
@@ -1,9 +1,13 @@
-package com.example.springbatch;
+package com.example.app;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableBatchProcessing
+@ComponentScan("com.example.config")
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
