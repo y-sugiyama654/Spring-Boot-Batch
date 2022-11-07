80f7d32c968bbd7a361dd72b55ca05b07af3230a
Yuta <y.sugiyama54@gmail.com>
2022-11-07 22:29:29 +0900

    First Tasklet Step

---
+++ src/main/java/com/example/config/SampleJob.java
@@ -0,0 +1,49 @@
+package com.example.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SampleJob {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job firstJob() {
+        return jobBuilderFactory
+                .get("First Job")
+                .start(firstStep())
+                .build();
+    }
+
+    private Step firstStep() {
+        return stepBuilderFactory
+                .get("First Step")
+                .tasklet(firstTask())
+                .build();
+    }
+
+    private Tasklet firstTask() {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("This is first tasklet step.");
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+
+}
